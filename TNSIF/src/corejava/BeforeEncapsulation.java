3414182687527a6cd4ae1e8a0e7a23a6375e1a12
dell <dell@DESKTOP-U784RGU>
2025-08-18 21:53:30 +0530

    commmit

---
+++ TNSIF/src/corejava/BeforeEncapsulation.java
@@ -0,0 +1,21 @@
+package corejava;
+
+class Human1
+{
+	int age;
+	String name;
+}
+
+
+public class BeforeEncapsulation {
+
+	public static void main(String[] args) {
+		Human1 obj=new Human1();
+		obj.age=26;
+		obj.name="hema";
+		obj.age=87;
+		obj.name="sushma";
+		System.out.println("Age " +obj.age + " "+ "Name "+obj.name);
+	}
+
+}
