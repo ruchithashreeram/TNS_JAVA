463d59ac56fbe4a34837ef37804d706d90bcdb59
dell <dell@DESKTOP-U784RGU>
2025-08-18 22:00:48 +0530

    commit

---
+++ TNSIF/src/corejava/MethodOverridingDemo.java
@@ -0,0 +1,53 @@
+package corejava;
+
+
+class Payment
+{
+	void makePayment()
+	{
+		System.out.println("Processing payment using generic method");
+	}
+}
+
+
+class UpiPayment extends  Payment
+{
+	void makePayment()
+	{
+		System.out.println("Payment made using UPI");
+	}
+}
+
+class CardPayment extends Payment
+{
+	void makePayment()
+	{
+		System.out.println("Payment made using Credit/debit card");
+	}
+}
+
+
+class Walletpayment extends Payment
+{
+	void makePayment()
+	{
+		System.out.println("Payment made using Digital Wallet");
+	}
+}
+
+public class MethodOverridingDemo {
+
+	public static void main(String[] args) {
+		Payment payment;//reference of parent class
+		
+		payment=new UpiPayment();
+		payment.makePayment();
+		
+		payment =new CardPayment();
+		payment.makePayment();
+		
+		payment=new Walletpayment();
+		payment.makePayment();
+	}
+
+}
